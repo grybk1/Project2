95c1c15ab7f40007168ea6a121f0a2f526223bc2
Tyler Kramer <kramer818@gmail.com>
2020-04-11 19:03:47 -0400

    end me pls

--- static/bubblechart.js
+++ static/bubblechart.js
@@ -1,22 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const url_bubble = "https://fathomless-badlands-97675.herokuapp.com/api/v1.0/MoviesByCountry";
-=======
-=======
->>>>>>> master
-<<<<<<< HEAD
-const url_bubble = "https://fathomless-badlands-97675.herokuapp.com/api/v1.0/MoviesByCountry";
-=======
-
-
-// const url = "https://fathomless-badlands-97675.herokuapp.com/api/v1.0/MoviesByCountry";
-
-const url_bubble = "http://localhost:5000/api/v1.0/MoviesByCountry";
->>>>>>> a237253b25a2082716bd7a39471538a95863f324
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
 
 
 

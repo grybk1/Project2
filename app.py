159ea5d68bb4d6f96283e5dae52cf12ba1ebdf6f
Lyndsi <lyndsi.wolf@gmail.com>
2020-04-11 12:44:50 -0400

    Heroku  stuff

--- app.py
+++ app.py
@@ -1,35 +1,77 @@
-import requests
-import json
-from pprint import pprint
-import csv
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+#from info import username,password
 
-title = []
-director = []
-year = []
-boxOffice = []
-location = []
-plot = []
 
-url = "http://www.omdbapi.com/?t="
-api_key = "&apikey="
+import matplotlib.pyplot as plt
+import numpy as np
+#from flask import Flask, jsonify
+from flask import Flask, render_template,jsonify
 
-with open('zombie.csv', newline='') as csvfile:
+connection = psycopg2.connect(user = "postgres",
+                                  password = "postgres",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "monsters")
 
-    csvreader = csv.reader(csvfile, delimiter = ',')
+cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
 
-    csv_header = next(csvreader)
 
-    for row in csvreader:
-        response = requests.get(url + row[1] + api_key)
+    # Print PostgreSQL version
+cursor.execute("SELECT version();")
+record = cursor.fetchone()
 
-        response_json = response.json()
+cursor.execute("select * from public.movies")
+allData=cursor.fetchall()
+allData_df = pd.DataFrame(allData)
 
-        try:
-            title.append(response_json['Title'])
-            director.append(response_json['Director'])
-            year.append(response_json['Year'])
-            boxOffice.append(response_json['BoxOffice'])
-            location.append(response_json['Country'])
-            plot.append(response_json['Plot'])
-        except:
-            pass
+cursor.execute('select C.name, count(*) from country C join xref_movie_country  on C.Id="countryId"  group by C.name  order by C.name')
+moviesByCountryData=cursor.fetchall()
+
+cursor.execute("select year, count(*) from movies group by year order by year")
+moviesByYearData=cursor.fetchall()
+
+cursor.execute("""select substring(year::text,1,3)||'0s', count(*) from movies
+group by substring(year::text,1,3)||'0s'
+order by substring(year::text,1,3)||'0s'""")
+moviesByDecadeData=cursor.fetchall()
+
+
+
+#print(allData)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", message="")
+
+
+@app.route("/api/v1.0/alldata")
+def allMovies():
+#    return allData.to_json()
+#    return allData_df.to_json()
+    return jsonify(allData)
+
+@app.route("/api/v1.0/MoviesByCountry")
+def MoviesByCountry():
+    return jsonify(moviesByCountryData)
+
+@app.route("/api/v1.0/MoviesByYear")
+def moviesByYear():
+    return jsonify(moviesByYearData)
+
+@app.route("/api/v1.0/MoviesByDecade")
+def moviesByDecade():
+    return jsonify(moviesByDecadeData)    
+
+
+# 4. Define main behavior
+if __name__ == "__main__":
+       app.run()
+    #    app.run(debug=True)
+
+
+
